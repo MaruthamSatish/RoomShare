b95c0ab0d44a710067248681f1242449b528ec12
satishmarutham <Satish@192.168.2.4>
2017-04-08 21:11:17 +0530

    test

--- RoomShare/src/main/java/com/roomshare/RoomShareApplication.java
+++ RoomShare/src/main/java/com/roomshare/RoomShareApplication.java
@@ -1,13 +1,21 @@
 package com.roomshare;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class RoomShareApplication {
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(RoomShareApplication.class);
+
      public static void main(String[] args)
      {
 		SpringApplication.run(RoomShareApplication.class, args);
+		log.info("In Spring Main");
 	 
      }
 }
